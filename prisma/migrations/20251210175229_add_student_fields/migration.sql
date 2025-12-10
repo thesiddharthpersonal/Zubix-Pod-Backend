84b1ec03be31f38a50b5e2559a02d69232b8c48a
siddharthsk0005 <siddharthsk0005@gmail.com>
2025-12-10 23:22:57 +0530

    feat: Add student fields (collegeName, currentCourse, yearSemester, interestDomain) to User model

---
+++ prisma/migrations/20251210175229_add_student_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "collegeName" TEXT,
+ADD COLUMN     "currentCourse" TEXT,
+ADD COLUMN     "interestDomain" TEXT,
+ADD COLUMN     "yearSemester" TEXT;
