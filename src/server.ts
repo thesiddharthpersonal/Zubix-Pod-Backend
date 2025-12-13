c5612c7d9eda663e81cc11cb09468bd3e9a40c56
siddharthsk0005 <siddharthsk0005@gmail.com>
2025-12-14 02:07:12 +0530

    fix: increase request body size limit to 50MB for large media uploads

--- src/server.ts
+++ src/server.ts
@@ -68,8 +68,8 @@ app.use((req: Request, res: Response, next: NextFunction) => {
   next();
 });
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 // Request logging middleware
 app.use((req: Request, res: Response, next: NextFunction) => {
